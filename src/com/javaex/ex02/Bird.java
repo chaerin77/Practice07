b6f977fea67ec559718da76832208b2a6c666c99
chaerin <newfkxpdlf@naver.com>
2021-12-03 16:18:57 +0900

    Practice 07

--- src/com/javaex/ex02/Bird.java
+++ src/com/javaex/ex02/Bird.java
@@ -1,8 +1,29 @@
 package com.javaex.ex02;
 
 public abstract class Bird {
-    private String name;
+	//필드
+    protected String name;
 
+    //생성자
+    public Bird() {
+    	
+    }
+
+    //메소드 g/s
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	//메소드 일반
+    public abstract void sing();
+    
+    public abstract void fly();
+    
+    public abstract void showName();
 
 
 }
